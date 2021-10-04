224f30d2afed252b1500466a1ef03949eb0535de
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-10-04 16:39:28 +0200

    Initial Commit

---
+++ Musik/MusikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusikApp.swift
+//  Musik
+//
+//  Created by Alessio Borraccino on 04.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MusikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
